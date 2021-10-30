12686964dc75d9a02817f2263707d5cc86a0fdf4
Brandon Cate <brandoncate95@gmail.com>
2021-10-30 16:32:53 -0500

    updates

--- src/main/kotlin/org/bloglog/processor/service/Extractor.kt
+++ src/main/kotlin/org/bloglog/processor/service/Extractor.kt
@@ -3,8 +3,10 @@ package org.bloglog.processor.service
 import com.google.gson.Gson
 import okhttp3.HttpUrl
 import okhttp3.HttpUrl.Companion.toHttpUrl
+import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
 import okhttp3.Request
+import okhttp3.RequestBody.Companion.toRequestBody
 import org.bloglog.processor.client.ignoreAllSSLErrors
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
@@ -39,6 +41,8 @@ class ExtractorService(
 ) : Extractor {
     private val client = OkHttpClient.Builder().ignoreAllSSLErrors().build()
 
+    private val json = "application/json; charset=utf-8".toMediaType()
+
     private val gson = Gson()
 
     @Throws(IOException::class)
@@ -46,12 +50,15 @@ class ExtractorService(
         val url = serviceUrl.toHttpUrl()
             .newBuilder()
             .addPathSegment("extract")
-            .addQueryParameter("repo", request.repo)
             .build()
+
+        val reqBody = gson.toJson(request, ExtractRequest::class.java).toRequestBody(json)
 
         val request: Request = Request.Builder()
             .url(url)
+            .post(reqBody)
             .build()
+
         val respBody = client.newCall(request).execute().body
 
         val tmp = respBody?.string()
